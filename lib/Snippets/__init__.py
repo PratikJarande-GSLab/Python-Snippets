42bce848b32d35b11adbe6e9d1e7e089e1c14b0d
JarandeP <pratik2992@gmail.com>
2016-04-23 17:51:39 +0530

    Fix tuple join for versionstr

--- lib/Snippets/__init__.py
+++ lib/Snippets/__init__.py
@@ -2,8 +2,8 @@ __author__ = 'Pratik Jarande'
 __name__ = 'Python Snippets'
 __url__ = 'https://github.com/JarandeP/PythonSnippets.git'
 __shortdoc__ = 'Python Snippets for quick referral'
-__version__ = tuple(1, 0, 1)
-__versionstr__ = '.'.join(__version__)
+__version__ = (1, 0, 1)
+__versionstr__ = '.'.join(map(str, __version__))
 
 locals()[__doc__] = __shortdoc__
 
